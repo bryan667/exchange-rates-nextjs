528a16f2e9abfa3ca872c7cf298fe6914d46bf33
bryan667 <janbryanmartirez@gmail.com>
2026-01-18 20:37:06 +0800

    feat:update curr in URL on dropdown change

--- components/BaseCurrencyDropdownMenu.tsx
+++ components/BaseCurrencyDropdownMenu.tsx
@@ -3,6 +3,7 @@ import React, { useMemo } from 'react';
 import { CurrencyOption } from '@/lib/types';
 import { defaultParameters } from '@/lib/helpers';
 import { CurrencySelect, SingleValue } from './SelectButton';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 type TProps = {
   selectedBaseCurrency: CurrencyOption;
@@ -14,6 +15,10 @@ export default function BaseCurrencyDropdownMenu(props: TProps) {
   const { selectedBaseCurrency, setSelectedBaseCurrency, allCurrencyOptions } =
     props;
 
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
   const baseCurrencyText = selectedBaseCurrency
     ? selectedBaseCurrency?.value?.toUpperCase()
     : '--';
@@ -23,6 +28,25 @@ export default function BaseCurrencyDropdownMenu(props: TProps) {
     (o) => o?.value === defaultParameters?.baseCurrency,
   );
 
+  const onDropDownMenuChange = (option: SingleValue<CurrencyOption>) => {
+    if (option !== null) {
+      setSelectedBaseCurrency({
+        label: option?.label,
+        value: option?.value,
+      });
+    }
+
+    const currency = option?.value.toLowerCase() || '';
+    const params = new URLSearchParams(searchParams);
+    if (currency) {
+      params.set('currency', currency);
+    } else {
+      params.delete('currency');
+    }
+
+    replace(`${pathname}?${params.toString()}`);
+  };
+
   return (
     <div className="min-w-[270px]">
       <div className="font-semibold">{`Base Currency: ${baseCurrencyText}`}</div>
@@ -31,14 +55,7 @@ export default function BaseCurrencyDropdownMenu(props: TProps) {
         classNamePrefix="select"
         defaultValue={defaultValue}
         value={selectedBaseCurrency}
-        onChange={(option: SingleValue<CurrencyOption>) => {
-          if (option !== null) {
-            setSelectedBaseCurrency({
-              label: option?.label,
-              value: option?.value,
-            });
-          }
-        }}
+        onChange={onDropDownMenuChange}
         isDisabled={false}
         isLoading={false}
         isSearchable={true}
